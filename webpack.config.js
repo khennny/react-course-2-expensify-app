f98c48d1730bd95310a0368d0781c99504ef200a
khennny <kennymagbagbeola@yahoo.com>
2018-01-07 13:04:15 +0100

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,53 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    //Set entry file and also location for the final bundled file
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    const CSSExtract = new ExtractTextPlugin('styles.css');
 
-    // Configure webpack to use babel loader for all .js files exempting the node_modules folder
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test:/\.s?css$/,                        
-            use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]       
-        }]
-    },
-    //The map file for displaying error messages properly in browser
-    devtool: 'cheap-module-eval-source-map',
-    // Configure webpack dev server
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+    return {
+        //Set entry file and also location for the final bundled file
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+    
+        // Configure webpack to use babel loader for all .js files exempting the node_modules folder
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{
+                test:/\.s?css$/,                        
+                use: CSSExtract.extract({
+                    use:[                        
+                        {
+                            loaders:'css-loader',
+                            options:{
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loaders: 'sass-loader',
+                            options:{
+                                sourceMap:true
+                            }
+                        }
+                    ]
+                })      
+            }]
+        },
+        plugins:[
+            CSSExtract
+        ],
+        //The map file for displaying error messages properly in browser
+        devtool: isProduction ? 'source-map' : 'inline-source-map',//'cheap-module-eval-source-map',
+        // Configure webpack dev server
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
